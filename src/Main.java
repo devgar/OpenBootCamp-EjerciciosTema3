ab615381126895fd0ac923ba1bb8b7e92ec871bc
Edgar Albalate <dev.gardo@gmail.com>
2022-07-13 00:00:28 +0200

    Implementa la función concatenar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+
+
+    /**
+     * Función que concatena las palabras de un array de Strings
+     * @param palabras Array de Strings con las palabras a ser concatenadas.
+     */
+    public static String concatenar(String [] palabras){
+        String resultado = "";
+        for (String palabra : palabras) {
+            resultado += palabra;
+        }
+        return resultado;
+    }
+    /**
+     * Función principal.
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] palabras = new String[]{"Hola", "Mundo", "!"};
+        System.out.println(concatenar(palabras));
     }
 }
